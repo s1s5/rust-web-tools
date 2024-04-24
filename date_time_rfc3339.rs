56672b3ce7b7164e5288ca13cfdf0ea88b712dce
Shogo Sawai <shogo.sawai@gmail.com>
2024-04-24 09:50:35 +0900

    make serializable and use utc

--- date_time_rfc3339.rs
+++ date_time_rfc3339.rs
@@ -1,9 +1,13 @@
 use async_graphql::{InputValueError, InputValueResult, Scalar, ScalarType, Value};
-pub struct DateTimeRfc3339(chrono::DateTime<chrono::FixedOffset>);
+use chrono::serde::ts_seconds;
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct DateTimeRfc3339(#[serde(with = "ts_seconds")] chrono::DateTime<chrono::Utc>);
 
 #[allow(dead_code)]
 impl DateTimeRfc3339 {
-    pub fn new(t: chrono::DateTime<chrono::FixedOffset>) -> DateTimeRfc3339 {
+    pub fn new(t: chrono::DateTime<chrono::Utc>) -> DateTimeRfc3339 {
         DateTimeRfc3339(t)
     }
 }
@@ -13,7 +17,7 @@ impl ScalarType for DateTimeRfc3339 {
     fn parse(value: Value) -> InputValueResult<Self> {
         if let Value::String(value) = &value {
             chrono::DateTime::parse_from_rfc3339(value)
-                .map(DateTimeRfc3339)
+                .map(|x| DateTimeRfc3339(x.into()))
                 .or(Err(InputValueError::custom("invalid rfc3339 format")))
         } else {
             Err(InputValueError::expected_type(value))
