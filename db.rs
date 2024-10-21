37c69762776ba11096385043253321c65804a002
Shogo Sawai <shogo.sawai@gmail.com>
2024-10-21 12:14:35 +0900

    :recycle: refactor db

--- db.rs
+++ db.rs
@@ -1,15 +1,15 @@
-use sea_orm::DatabaseConnection;
+use sea_orm::{ConnectionTrait, DatabaseConnection};
 use std::sync::Arc;
 
 use async_graphql::{dataloader::DataLoader, Context};
 
 type Result<T> = anyhow::Result<T>;
 
-pub struct Database {
-    pub connection: Arc<DatabaseConnection>,
+pub struct Database<C> {
+    pub connection: Arc<C>,
 }
 
-impl Database {
+impl Database<DatabaseConnection> {
     pub async fn new_from_env() -> Result<DataLoader<Self>> {
         Ok(DataLoader::new(
             Self {
@@ -20,15 +20,31 @@ impl Database {
             tokio::task::spawn,
         ))
     }
+}
+
+impl<C> Database<C>
+where
+    C: ConnectionTrait,
+{
+    pub fn new(connection: C) -> DataLoader<Self> {
+        DataLoader::new(
+            Self {
+                connection: Arc::new(connection),
+            },
+            tokio::task::spawn,
+        )
+    }
 
     #[inline]
-    pub fn get_connection(&self) -> &DatabaseConnection {
+    pub fn get_connection(&self) -> &C {
         &self.connection
     }
 }
 
-pub fn get_data_loader_from_ctx<'a>(ctx: &Context<'a>) -> &'a DataLoader<Database> {
-    ctx.data_unchecked::<DataLoader<Database>>()
+pub fn get_data_loader_from_ctx<'a>(
+    ctx: &Context<'a>,
+) -> &'a DataLoader<Database<DatabaseConnection>> {
+    ctx.data_unchecked::<DataLoader<Database<DatabaseConnection>>>()
 }
 
 pub fn get_db_from_ctx<'a>(ctx: &Context<'a>) -> &'a DatabaseConnection {
