a278ebe9960d568d1c57b92010b5530e7a0cd6ff
Shogo Sawai <shogo.sawai@gmail.com>
2024-10-22 13:20:50 +0900

    :sparkles: add bulk_update

--- db.rs
+++ db.rs
@@ -1,7 +1,10 @@
-use sea_orm::{ConnectionTrait, DatabaseConnection};
-use std::sync::Arc;
-
 use async_graphql::{dataloader::DataLoader, Context};
+use sea_orm::{
+    prelude::Expr, sea_query::CaseStatement, ActiveModelTrait, ColumnTrait, Condition,
+    ConnectionTrait, DatabaseConnection, EntityTrait, Iterable, PrimaryKeyToColumn, QueryFilter,
+    UpdateMany,
+};
+use std::sync::Arc;
 
 type Result<T> = anyhow::Result<T>;
 
@@ -49,4 +52,38 @@ pub fn get_data_loader_from_ctx<'a>(
 
 pub fn get_db_from_ctx<'a>(ctx: &Context<'a>) -> &'a DatabaseConnection {
     get_data_loader_from_ctx(ctx).loader().get_connection()
+}
+
+/// batch update
+/// bulk_update(&[a], &[Column::Name]).exec(conn).await?;
+pub fn bulk_update<A>(
+    objects: &[A],
+    fields: &[<A::Entity as EntityTrait>::Column],
+) -> Result<UpdateMany<A::Entity>>
+where
+    A: ActiveModelTrait,
+{
+    let mut cond_list = vec![];
+    let mut filter_cond = Condition::any();
+    for obj in objects.iter() {
+        let mut cond = Condition::all();
+        for pk in <A::Entity as EntityTrait>::PrimaryKey::iter() {
+            cond = cond.add(pk.into_column().eq(obj.get(pk.into_column()).unwrap()));
+        }
+        cond_list.push(cond.clone());
+        filter_cond = filter_cond.add(cond);
+    }
+
+    let mut qs = <A::Entity as EntityTrait>::update_many().filter(filter_cond);
+    for f in fields {
+        let case_statement = objects
+            .iter()
+            .zip(cond_list.iter())
+            .fold(CaseStatement::new(), |a, (e, c)| {
+                a.case(c.clone(), Expr::val(e.get(*f).unwrap()))
+            });
+        qs = qs.col_expr(*f, case_statement.into())
+    }
+
+    Ok(qs)
 }
